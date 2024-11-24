3aef17241cc391dfb9b258dd904d53938fbe7885
Franklin Shera <fshera96@gmail.com>
2024-11-25 00:04:59 +0300

    tsconfig paths

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { vitePlugin as remix } from '@remix-run/dev';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [remix()],
-  server: {
-    host: '127.0.0.1',
-  },
-});
